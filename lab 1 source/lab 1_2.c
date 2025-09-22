a51527eae6fc69c52b384d62f580b579f7e10371
Nikita Serikov <31020070+jerichofox@users.noreply.github.com>
2025-09-22 23:03:06 +0300

    Add files via upload

---
+++ lab 1 source/lab 1_2.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void main() 
+{
+	setlocale(LC_ALL, "RUS");
+
+	puts("\n\n");
+	puts("\t\t* * * * * * * * * * * * * * * * * * * * * *");
+	puts("\t\t*                                         *");
+	puts("\t\t* тема: Разработка консольного приложения *");
+	puts("\t\t*                                         *");
+	puts("\t\t*     Выполнил Сериков Н.А.               *");
+	puts("\t\t*                                         *");
+	puts("\t\t* * * * * * * * * * * * * * * * * * * * * *");
+	puts("\n");	
+
+	return 0;
+}
